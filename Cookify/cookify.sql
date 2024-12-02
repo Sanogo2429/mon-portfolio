05a0fb3ba096ae2232f4ed8bc8dfba2c6066a95d
ZOGLO YAWO AKOUETE VALERE PIERRE <zoglo@et.esiea.fr>
2024-12-02 14:03:44 +0100

    mis à jour du schema de BDD

--- Cookify/cookify.sql
+++ Cookify/cookify.sql
@@ -9,21 +9,92 @@ nom string
 );
 create table type (
 id int primary key auto_increment, 
-nom string   
-
+nom string,  
+id_pays int not null,
+id_pays foreign key references pays(id)
 );
 
-
-
-create table pays_type(
-id_pays int not null,
-id_type int not null,
-foreign key(id_pays) references pays(id),
-foreign key(id_type) references type (id)
-)
 create table sous_type (
     id int primary key auto_increment,
-) 
+    id_type int,
+    id_type foreign key references types(id)
+);
 create table recette(
-    id 
+    id int primary key auto_increment,
+'nom' string,
+'description' text,
+'image' varchar(255),
+ingredients varchar(255),
+temps_preparation int,
+id_pays int not null,
+id_type int not null,
+id_sous_type int not null,
+id_pays foreign key references pays(id),
+id_type foreign key references type(id),
+id_sous_type foreign key references sous_type(id)
+);
+create table livre (
+    id int primary key auto_increment,
+    titre varchar(255),
+    nombre_page int,
+    annee_publication datetime
 )
+create table recette_favori(
+    id int primary key auto_increment,
+    id_recette int not null,
+    id_utilisateur int not null,
+    id_recette  foreign key references recette (id),
+    id_utilisateur foreign key references utilisateur(id)
+);
+
+create table livre_favori(
+    id int primary key auto_increment,
+    id_livre int not null,
+    id_utilisateur int not null,
+    id_livre  foreign key references livre (id),
+    id_utilisateur foreign key references utilisateur(id) 
+);
+create table commentaire (
+    id int primary key,
+    id_recette int not null,
+     id_utlisateur int not null,
+     id_recette foreign key references recette(id),
+     id_utlisateur foreign key references utilisateur (id)
+)
+create table utilisateur (
+    id int primary key auto_increment,
+    nom varchar (255),
+    prenom varchar(255),
+    email varchar(255),
+    mot_de_passe varchar(255),
+    pseudo varchar(255),
+    id_recette_favori int, 
+    id_livre_favori int, 
+    id_commentaire int,
+    id_recette_favori foreign key references recette_favori(id),
+id_livre_favori foreign key references livre_favori(id),
+id_commentaire foreign key references commentaire (id)
+);
+create table categorie(
+    id int primary key auto_increment,
+    titre varchar(255),
+)
+create table item (
+    id int primary key auto_increment,
+    id_categorie int not null, 
+    text proposition,
+)
+create table reponse_proposition (
+    id int primary key auto_increment,
+    id_item int not null,
+id_item foreign key references item (id)
+)
+create table reponse_utilisateur (
+    id int primary key auto_increment,
+    id_item int not null,
+     id_reponse_utlisateur int not null, 
+    id_utlisateur int not null, 
+   id_item foreign key references item (id),id_reponse_utlisateur foreign key references reponse_utlisateur(id),
+   id_utlisateur foreign key references utlisateur (id)
+);
+
